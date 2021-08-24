0f535c05308f2d61bb675ffa63782f6c111ea8af
laurabrennanQ4 <laura.brennan@q4inc.com>
2021-08-24 14:19:13 -0400

    ready for assessment

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,14 @@ var reportsWidget = {
         containerSelector: '.reports',
         template: (
             '{{#.}}' +
-                '<article class="reports_item">' +
+                '<article class="reports_item col_1-of-3 col_1-of-2">' +
                     '<a href="{{cover}}" target="_blank">' +
                         '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
                     '</a>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
                             '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
+                                '<a href="{{url}}" target="_blank">{{title}} <span class="reports_data">({{file_size}} {{file_type}})</span></a>' +
                             '</h3>' +
                         '{{/documents}}' +
                     '</footer>' +
