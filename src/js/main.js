df6524c22038717a21508a5d43d91c2f6568b21d
laurabrennanQ4 <laura.brennan@q4inc.com>
2021-08-24 14:37:18 -0400

    added mobile phone breakpoint to css

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ var reportsWidget = {
         containerSelector: '.reports',
         template: (
             '{{#.}}' +
-                '<article class="reports_item col_1-of-3 col_1-of-2">' +
+                '<article class="reports_item col_1-of-3--lg col_1-of-2--md col_1-of-1--sm">' +
                     '<a href="{{cover}}" target="_blank">' +
                         '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
                     '</a>' +
